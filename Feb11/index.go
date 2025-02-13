009743b5dfc4f6585078623125362cf9211686ec
Raelyn Brooks <rbrooks27@wooster.edu>
2025-02-13 11:28:07 -0500

    Feb11

---
+++ Feb11/index.go
@@ -0,0 +1,13 @@
+// Package Declaration (main func included) (it is mandatory)
+package main
+
+// import packages(it is mandatory)
+import "fmt"
+
+// function (it is mandatory)
+func main() {
+
+    // statements and expressions 
+    fmt.Println("Hello World!")
+
+}
