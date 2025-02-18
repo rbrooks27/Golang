01745985389acc0e4b472aa14e3d9a908d3ce9bb
Raelyn Brooks <rbrooks27@wooster.edu>
2025-02-18 13:24:34 -0500

    array

--- Feb18/array.go
+++ Feb18/array.go
@@ -34,3 +34,113 @@ Output:
 [4 5 6 7 8]
 */
 }
+
+/*
+Array of Strings:
+
+package main
+import ("fmt")
+
+func main() {
+  var cars = [4]string{"Volvo", "BMW", "Ford", "Mazda"}
+  fmt.Print(cars)
+}
+
+//Output: [Volvo BMW Ford Mazda]
+
+
+*/
+
+/*
+Accessing Elements of Array:
+
+package main
+import ("fmt")
+
+func main() {
+  prices := [3]int{10,20,30}
+
+  fmt.Println(prices[0])
+  fmt.Println(prices[2])
+}
+
+// Output: 
+// 10 
+// 30
+
+*/
+
+/*
+Changing Elements of an Array:
+
+package main
+import ("fmt")
+
+func main() {
+  prices := [3]int{10,20,30}
+
+  prices[2] = 50
+  fmt.Println(prices)
+}
+
+// Output: [10, 20, 50]
+
+*/
+
+/*
+Array Initialization:
+
+package main
+import ("fmt")
+
+func main() {
+  arr1 := [5]int{} //not initialized
+  arr2 := [5]int{1,2} //partially initialized
+  arr3 := [5]int{1,2,3,4,5} //fully initialized
+
+  fmt.Println(arr1)
+  fmt.Println(arr2)
+  fmt.Println(arr3)
+}
+
+// Output: 
+// [0 0 0 0 0]
+// [1 2 0 0 0]
+// [1 2 3 4 5]
+
+*/
+
+/*
+Initialize Only Specific Elements:
+
+package main
+import ("fmt")
+
+func main() {
+  arr1 := [5]int{1:10,2:40}
+
+  fmt.Println(arr1)
+}
+
+// Output: [0 10 40 0 0]
+
+*/
+
+/*
+Finding the Length of an Array:
+
+package main
+import ("fmt")
+
+func main() {
+  arr1 := [4]string{"Volvo", "BMW", "Ford", "Mazda"}
+  arr2 := [...]int{1,2,3,4,5,6}
+
+  fmt.Println(len(arr1))
+  fmt.Println(len(arr2))
+}
+
+// Output: 4 6
+*/
+
+
