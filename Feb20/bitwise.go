5b03b34c4d6792fa5163d53f97ae97087c2d1ad5
Raelyn Brooks <rbrooks27@wooster.edu>
2025-02-20 13:18:09 -0500

    h

--- Feb20/bitwise.go
+++ Feb20/bitwise.go
@@ -22,11 +22,11 @@ func main() {
 }
 
 /*
-Operator		Name						Description																Example
-& 				AND							Sets each bit to 1 if both bits are 1									x & y	
-|				OR							Sets each bit to 1 if one of two bits is 1								x | y	
- ^				XOR							Sets each bit to 1 if only one of two bits is 1							x ^ b	
-<<				Zero fill left shift		Shift left by pushing zeros in from the right							x << 2	
->>				Signed right shift			Shift right by pushing copies of the leftmost bit in from the left, 	x >> 2
-											and let the rightmost bits fall off	
+Operator		Name						        Description																                            Example
+& 				  AND							        Sets each bit to 1 if both bits are 1									                  x & y	
+|				    OR							        Sets each bit to 1 if one of two bits is 1							               	x | y	
+ ^				  XOR							        Sets each bit to 1 if only one of two bits is 1							            x ^ b	
+<<				  Zero fill left shift		Shift left by pushing zeros in from the right							              x << 2	
+>>				  Signed right shift			Shift right by pushing copies of the leftmost bit in from the left, 	  x >> 2
+											              and let the rightmost bits fall off	
 */
